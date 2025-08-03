6c6fd8d2606802ac2e8ee9fb200e6b1636679bde
Sander Roosendaal <sanderroosendaal@proton.me>
2025-08-03 19:59:22 +0200

    arrays with magicl

--- std/arrays.lisp
+++ std/arrays.lisp
@@ -163,3 +163,32 @@
 (register-op 'idx #'indexof 2)
 (register-op 'flatten #'flatten 1)
 (register-op 'rank #'rank-fn 1)
+
+;; Magicl stuff
+(ql:quickload :magicl)
+
+(defun array-to-magicl-fast (arr)
+  (let* ((dims (array-dimensions arr))
+	 (flat-data (map 'vector
+			 (lambda (x) (coerce x 'double-float))
+			 (make-array (array-total-size arr) :displaced-to arr))))
+    (magicl:from-list (coerce flat-data 'list) dims)))
+
+(defun magicl-matrix-to-array (magicl-matrix)
+  (let* ((dims (magicl:shape magicl-matrix))
+	 (result (make-array dims :element-type 'double-float)))
+    (dotimes (i (first dims))
+      (dotimes (j (second dims))
+	(setf (aref result i j)
+	      (magicl:tref magicl-matrix i j))))
+    result))
+
+(defun matrix-multiply (a b)
+  "Multiply two matrices A and B using Magicl."
+  (let* ((magicl-a (array-to-magicl-fast a))
+	 (magicl-b (array-to-magicl-fast b))
+	 (result-magicl (magicl:@ magicl-a magicl-b)))
+    (magicl-matrix-to-array result-magicl)))
+
+(register-op 'mmult #'matrix-multiply 2)
+(register-op '@ #'matrix-multiply 2)
